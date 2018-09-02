cb3429974abbd53313dcaac8cf41520c0afba28a
Smashedbotatos <ian@icarey.net>
2018-09-02 12:16:54 -0400

    Initial Commit
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ src/main/java/net/icarey/deathpenalty/DeathPenalty.java
@@ -0,0 +1,17 @@
+package net.icarey.deathpenalty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathPenalty extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
